f4ab1b12e8a23ba71460be98a4b39a472740be19
Peter Meißner <retep.meissner@gmail.com>
2024-08-27 08:28:59 +0200

    init

---
+++ src/main/java/de/petermeissner/restjms19/HelloApplication.java
@@ -0,0 +1,9 @@
+package de.petermeissner.restjms19;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
